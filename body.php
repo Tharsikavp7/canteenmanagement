343e3d137651ed08bc6398bd263d702c9eb223e6
Tharsika Puvanendiran <146452153+Tharsikavp7@users.noreply.github.com>
2024-12-31 11:13:39 +0530

    Add files via upload

---
+++ body.php
@@ -0,0 +1,12 @@
+<div class="container d-flex justify-content-center mt-4">
+  <div class="col-md-4"> <a href="index.php?page=menu.php" class="btn btn-primary btn-lg d-flex align-items-center justify-content-center">
+      <i class="fas fa-utensils me-2"></i>
+      Menu and Order
+    </a>
+  </div>
+  <div class="col-md-4 ms-4"> <a href="index.php?page=feedback.php" class="btn btn-secondary btn-lg d-flex align-items-center justify-content-center">
+      <i class="fas fa-comments me-2"></i>
+      Feedback
+    </a>
+  </div>
+</div>
